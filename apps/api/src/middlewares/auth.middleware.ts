9d466f35551049935b76c2f4e3912882f71129ca
NiaikoLucka <ranarisonlucka@gmail.com>
2026-08-01 23:32:42 +0300

    Feat: add a middleware for auth

---
+++ apps/api/src/middlewares/auth.middleware.ts
@@ -0,0 +1,30 @@
+import type { Request, Response, NextFunction } from "express";
+import { auth } from "../auth/auth.js";
+
+export interface AuthRequest extends Request {
+  user?: {
+    id: string;
+    email: string;
+    name: string;
+  };
+}
+
+export async function requireAuth(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) {
+  const session = await auth.api.getSession({
+    headers: req.headers as unknown as Headers,
+  });
+
+  if (!session) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  req.user = session.user;
+
+  next();
+}
