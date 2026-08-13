17d08aaf36509b4a8a71290f0c98941b08e85ca7
NiaikoLucka <ranarisonlucka@gmail.com>
2026-08-14 01:05:19 +0300

    Feat: Configure Axios

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,6 @@
 const App = () => {
+  console.log(import.meta.env);
+  
   return (
     <div className="flex items-center justify-center w-full h-screen bg-background">
       <h1 className="font-bold text-5xl">Hello world</h1>
